b50656e2ac302eee540f7d01272d6c53ca29857d
Pixdigit <max12354678910@gmail.com>
2017-06-22 16:36:14 +0200

    maybethis tm?

--- listener.py
+++ listener.py
@@ -7,7 +7,7 @@ update_queue = queue.Queue()
 
 def init(bot):
     global server
-    server = webhookhandler.WebhookServer(("127.0.0.1", 80), webhookhandler.WebhookHandler, update_queue, config.token, bot)
+    server = webhookhandler.WebhookServer(("localhost", 80), webhookhandler.WebhookHandler, update_queue, config.token, bot)
     server.serve_forever()
 
 def get_updates():
