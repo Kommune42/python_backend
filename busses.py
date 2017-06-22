59d0a6a2b1726fe2ba911a2865e2734ee71d9290
Pixdigit <max12354678910@gmail.com>
2017-06-22 17:40:54 +0200

    small tester

--- busses.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-global conversation_bus
-global status_bus
-
-conversation_bus = {}  # chat_id: state
-status_bus = {"station": 0}
--- main.py
+++ main.py
@@ -1,34 +1,9 @@
-# -*- coding: utf-8 -*-
+import os
 import telegram
-import config
-import helper
-import listener
-import busses
-import response_engine
 
+token = os.environ.get("TOKEN")
 
-saufi = telegram.Bot(token=config.token)
-saufi.set_webhook(url=(config.webhook_address + "/" + config.token))
-listener.init(saufi)
+botti = telegram.Bot(token=token)
 
-
-for _ in range(500000):
-    for update in listener.get_updates():
-        #Some updates do not have a message
-        if update.message is not None:
-            msg = update.message
-            msg_type = helper.get_message_type(msg)
-
-            if not msg.chat.id in busses.conversation_bus:
-                busses.conversation_bus[msg.chat.id] = {"state": None, "user": 0}
-
-            if msg_type == "location":
-                response_engine.location_handler(msg)
-
-            if msg_type == "text":
-                if msg.text[0] == "/":
-                    response_engine.command_handler(msg)
-                else:
-                    response_engine.text_handler(msg)
-        elif update.inline_query is not None:
-            response_engine.inline_handler(update.inline_query)
+while True:
+	print botti.get_updates()
