786123ff05edb846148591bea45ff0eb3d1dd905
Pixdigit <max12354678910@gmail.com>
2017-06-22 16:30:39 +0200

    new day new hope

--- listener.py
+++ listener.py
@@ -7,7 +7,7 @@ update_queue = queue.Queue()
 
 def init(bot):
     global server
-    server = webhookhandler.WebhookServer((config.webhook_address, 80), webhookhandler.WebhookHandler, update_queue, config.token, bot)
+    server = webhookhandler.WebhookServer(("0.0.0.0", 80), webhookhandler.WebhookHandler, update_queue, config.token, bot)
     server.serve_forever()
 
 def get_updates():
